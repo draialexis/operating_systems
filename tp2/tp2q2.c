160a7126504c25d3980396aacd1e2472737b33d2
Alexis DRAI <drai.alexis@gmail.com>
2022-05-07 16:17:25 +0200

    Prettify code

--- tp2/tp2q2.c
+++ tp2/tp2q2.c
@@ -3,40 +3,46 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-void f(int sig){
-  int status;
-  printf("zombie child deleted = %d\n", wait(&status));
+void f(int sig)
+{
+    int status;
+    printf("zombie child deleted = %d\n", wait(&status));
 }
 
-void derail(int sig){
-  struct sigaction handler;
-  handler.sa_handler = f;
-  printf("signal %d received and derailed by parent\n", sig);
-  if(sigaction(sig, &handler, NULL) == -1){
-    perror("sigaction error\n");
-    exit(-1);
-  }
-}
-
-int main(){
-  pid_t p;
-  derail(SIGCHLD);
-  p = fork();
-
-  switch(p){
-  case 0:
-    printf("child %d\n", getpid());
-    //SIGCHLD is sent to the parent process
-    //f is called because of the sigaction() call in derail()
-    break;
-  case -1:
-    perror("fork error\n");
-    exit(-1);
-  default:
-    while(1){
-      printf("working...\n");
-      sleep(1);
+void derail(int sig)
+{
+    struct sigaction handler;
+    handler.sa_handler = f;
+    printf("signal %d received and derailed by parent\n", sig);
+    if (sigaction(sig, &handler, NULL) == -1)
+    {
+        perror("sigaction error\n");
+        exit(-1);
     }
-  }
-  return 0;
+}
+
+int main()
+{
+    pid_t p;
+    derail(SIGCHLD);
+    p = fork();
+
+    switch (p)
+    {
+        case 0:
+            printf("child %d\n", getpid());
+            //SIGCHLD is sent to the parent process
+            //f is called because of the sigaction() call in derail()
+            break;
+        case -1:
+            perror("fork error\n");
+            exit(-1);
+        default:
+            while (1)
+            {
+                printf("working...\n");
+                sleep(1);
+            }
+    }
+    return 0;
 }
--- tp2/tp2q3.c
+++ tp2/tp2q3.c
@@ -4,34 +4,36 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-int main(){
-  pid_t p;
-  int status;
-  printf("starting...\n");
-  p = fork();
-  switch(p){
-  case 0:
-    printf("child 1\n");
-    sleep(1);
-    kill(getppid(), SIGCONT);
-    kill(getpid(), SIGSTOP);
-    printf("child 2\n");
-    sleep(1);
-    kill(getppid(), SIGCONT);
-    break;
-  case -1:
-    perror("fork error\n");
-    exit(-1);
-  default:
-    kill(getpid(), SIGSTOP);
-    printf("parent 1\n");
-    sleep(1);
-    kill(p, SIGCONT);
-    kill(getpid(), SIGSTOP);
-    printf("parent 2\n");
-    wait(&status);
-    exit(0);
-    break;
-  }
-  return 0;
+int main()
+{
+    pid_t p;
+    int   status;
+    printf("starting...\n");
+    p = fork();
+    switch (p)
+    {
+        case 0:
+            printf("child 1\n");
+            sleep(1);
+            kill(getppid(), SIGCONT);
+            kill(getpid(), SIGSTOP);
+            printf("child 2\n");
+            sleep(1);
+            kill(getppid(), SIGCONT);
+            break;
+        case -1:
+            perror("fork error\n");
+            exit(-1);
+        default:
+            kill(getpid(), SIGSTOP);
+            printf("parent 1\n");
+            sleep(1);
+            kill(p, SIGCONT);
+            kill(getpid(), SIGSTOP);
+            printf("parent 2\n");
+            wait(&status);
+            exit(0);
+            break;
+    }
+    return 0;
 }
